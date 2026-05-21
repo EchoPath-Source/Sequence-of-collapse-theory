5db5119b814ba587cf35c5485b69353641370fad
EchoPath XR <echopathxr@gmail.com>
2026-05-21 08:53:17 -0400

    Add visualization entrypoint for PNT Hubble void dashboard

---
+++ visualizations/pnt_hubble_void.jsx
@@ -0,0 +1,4 @@
+// Visualization entrypoint for the PNT void-filament H0 dashboard.
+// Canonical implementation lives in simulations/pnt_hubble_void/pnt_hubble_void.jsx.
+
+export { default } from "../simulations/pnt_hubble_void/pnt_hubble_void.jsx";
